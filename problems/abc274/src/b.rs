77a5e9268135dfdfea2487524747a791a3115fde
sumirechan0341 <mazeppa1108@yahoo.co.jp>
2023-06-03 01:33:42 +0900

    解いたつもりになっていたやつを解いた(0問追加)

--- problems/abc274/src/b.rs
+++ problems/abc274/src/b.rs
@@ -2,6 +2,17 @@ use proconio::{input, marker::Chars};
 
 pub fn main() {
     input! {
-        
+        h: usize,
+        w: usize,
+        chw: [Chars; h]
     };
+    let mut counts = vec![0; w];
+    for i in 0..h {
+        for j in 0..w {
+            if chw[i][j] == '#' {
+                counts[j] += 1;
+            }
+        }
+    }
+    println!("{}", counts.iter().map(|x| x.to_string()).collect::<Vec<_>>().join(" "));
 }
