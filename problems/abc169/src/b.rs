2fae2fcd8d15352ba1680ef59dbf01330c6f4f30
sumirechan0341 <mazeppa1108@yahoo.co.jp>
2023-06-08 19:38:50 +0900

    直した

--- problems/abc169/src/b.rs
+++ problems/abc169/src/b.rs
@@ -2,6 +2,22 @@ use proconio::{input, marker::Chars};
 
 pub fn main() {
     input! {
-        
+        n: usize,
+        mut an: [u128; n]
     };
+    an.sort();
+    an.reverse();
+    if an[n-1] == 0 {
+        println!("{}", 0);
+        return;
+    }
+    let mut acc = 1;
+    for a in an {
+        acc *= a;
+        if acc > 10_u128.pow(18) {
+            println!("{}", -1);
+            return;
+        }
+    }
+    println!("{}", acc);
 }
