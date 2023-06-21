b3d2b8bb7df8322cbefa82d966ae7bb68b516241
sumirechan0341 <mazeppa1108@yahoo.co.jp>
2023-06-22 01:25:11 +0900

    1問追加 DFSきつい…

--- problems/abc270/src/c.rs
+++ problems/abc270/src/c.rs
@@ -14,30 +14,27 @@ pub fn main() {
         graph[u-1].push(v-1);
         graph[v-1].push(u-1);
     }
-    let mut used = vec![false; n];
-    let mut q = VecDeque::<usize>::new();
-    q.push_back(y-1);
-    let mut path = VecDeque::<Vec<String>>::new();
-    let mut ans = vec![];
-    path.push_back(vec![]);
-    while !q.is_empty() {
-        let current = q.pop_front().unwrap();
-        let p = &mut path.pop_front().unwrap();
-        // let mut local_path = vec![];
-        p.push((current+1).to_string());
-        used[current] = true;
-        if current == x-1 {
-            ans = p.to_vec();
-            break;
-        }
-        for next in &graph[current] {
-            if !used[*next] {
-                path.push_front(p.to_vec());
-                q.push_front(*next);
-                used[*next] = true;
+    let used = &mut vec![false; n];
+    let path = &mut VecDeque::<usize>::new();
+    dfs(x-1, y-1, &graph, used, path);
+    println!("{}", path.into_iter().map(|x| x.to_string()).collect::<Vec<_>>().join(" "));
+}
+
+fn dfs(current: usize, goal: usize, graph: &Vec<Vec<usize>>, used: &mut Vec<bool>, path: &mut VecDeque<usize>) -> bool {
+    used[current] = true;
+    path.push_back(current+1);
+    if current == goal {
+        return true;
+    }
+    
+    for &next in &graph[current] {
+        if !used[next] {
+            used[next] = true;
+            if dfs(next, goal, graph, used, path) {
+                return true;
             }
         }
     }
-    ans.reverse();
-    println!("{}", ans.join(" "));
+    path.pop_back();
+    return false;
 }
