578f12264071517bb7debedb958268917d3c6858
sumirechan0341 <mazeppa1108@yahoo.co.jp>
2023-07-05 10:53:15 +0900

    1問追加

--- problems/abc204/src/c.rs
+++ problems/abc204/src/c.rs
@@ -1,3 +1,5 @@
+use std::collections::{VecDeque, HashSet};
+
 use proconio::{input, marker::Chars};
 type VS = Vec<String>;
 
@@ -5,6 +7,31 @@ pub fn main() {
     input!{
         n: usize,
         m: usize,
-        abm: [(usize, usize); m]
+        mut abm: [(usize, usize); m]
     };
+    let mut graph = vec![vec![]; n];
+    for i in 0..m {
+        graph[abm[i].0-1].push(abm[i].1-1);
+    }
+    let mut count = HashSet::<(usize, usize)>::new();
+    for i in 0..n {
+        let mut used = vec![false; n];
+        let mut q = VecDeque::<usize>::new();
+        q.push_back(i);
+        while !q.is_empty() {
+            let current = q.pop_front().unwrap();
+            if used[current] {
+                continue;
+            }
+            used[current] = true;
+            count.insert((i, current));
+            for j in &graph[current] {
+                if used[*j] {
+                    continue;
+                }
+                q.push_front(*j);
+            }
+        }
+    }
+    println!("{}", count.len());
 }
