9ea8f7d35a526195c1156f27cc7ddf2af9ebfa18
sumirechan0341 <mazeppa1108@yahoo.co.jp>
2023-07-26 00:47:39 +0900

    1問追加

--- problems/abc119/src/c.rs
+++ problems/abc119/src/c.rs
@@ -12,31 +12,35 @@ pub fn main() {
     // つなげる、伸ばす、縮めるは可換
     for i in 0..4_i32.pow(n as u32) {
         let mut ii = i;
-        // つなげる方を全探索する
+        // つなげ方を全探索する
         // 使わないパターンもあるので4のn乗
         let mut total = vec![0; 3];
         let mut cost = 0;
-        let mut remain = vec![];
         for j in 0..n {
             if ii % 4 == 0 {
+                if total[0] != 0 {
+                    cost += 10;
+                }
                 total[0] += ln[j];
-                cost += 10;
             } else if ii % 4 == 1 {
+                if total[1] != 0 {
+                    cost += 10;
+                }
                 total[1] += ln[j];
-                cost += 10;
             } else if ii % 4 == 2 {
+                if total[2] != 0 {
+                    cost += 10;
+                }
                 total[2] += ln[j];
-                cost += 10;
             } else {
-                remain.push(ln[j]);
             }
             ii /= 4;
         }
         if total[0] == 0 || total[1] == 0 || total[2] == 0 {
             continue;
         } 
-        for c in (0..3).permutations(3) {
-            let local = (a - total[c[0]]).abs() + (b - total[c[1]]).abs() + (b - total[c[2]]).abs();
+        for comb in (0..3).permutations(3) {
+            let local = (a - total[comb[0]]).abs() + (b - total[comb[1]]).abs() + (c - total[comb[2]]).abs();
             if local + cost < min {
                 min = local + cost;
             }
