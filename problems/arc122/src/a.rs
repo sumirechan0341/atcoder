235ad08feedad119a27ee3c37289f9b903bf59cf
sumirechan0341 <mazeppa1108@yahoo.co.jp>
2023-08-31 01:14:19 +0900

    もうちょっと修正

--- problems/arc122/src/a.rs
+++ problems/arc122/src/a.rs
@@ -8,9 +8,12 @@ pub fn main() {
     };
     let p = 1000000007;
     let mut dp = vec![0; n];
-    for i in (0..n).rev() {
-
+    dp[0] = an[n-1];
+    dp[1] = 2*an[n-2];
+    for i in 2..n {
+        dp[i] = an[n-i-1]+dp[i-1] + (an[n-i-1]-an[n-i])+dp[i-2];
     }
+    println!("{:?}", dp);
     // メモ化再帰みたいなの必要な気がする
     // 3 + dp[2] + (3-1) + dp[1] = 3 + 2 + 3 -5
 }
