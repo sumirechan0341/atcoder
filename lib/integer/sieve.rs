46b0703d52c541d22e33023496954158ba4c8cea
sumirechan0341 <mazeppa1108@yahoo.co.jp>
2023-09-28 11:04:57 +0900

    エラトステネスの篩実装

---
+++ lib/integer/sieve.rs
@@ -0,0 +1,39 @@
+fn sieve(n: i64) -> Vec<i64> {
+    let prime = vec![true; (n+1) as usize];
+    let mut res = vec![];
+    if n >= 0 {
+        prime[0] = false;
+    }
+    if n >= 1 {
+        prime[1] = false;
+    }
+    for i in 2..=n {
+        if !prime[i as usize] {
+            continue;
+        }
+        res.push(i);
+        for j in (i+1..=n).step_by(i as usize) {
+            prime[j as usize] = false;
+        }
+    }
+    return res;
+}
+
+fn is_prime_table(n: i64) -> Vec<i64> {
+    let prime = vec![true; (n+1) as usize];
+    if n >= 0 {
+        prime[0] = false;
+    }
+    if n >= 1 {
+        prime[1] = false;
+    }
+    for i in 2..=n {
+        if !prime[i as usize] {
+            continue;
+        }
+        for j in (i+1..=n).step_by(i as usize) {
+            prime[j as usize] = false;
+        }
+    }
+    return is_prime_table;
+}
