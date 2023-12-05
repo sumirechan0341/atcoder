6ec3b33dcf97402b1c54ab1e59b8016523a750fe
sumirechan0341 <mazeppa1108@yahoo.co.jp>
2023-12-05 13:00:38 +0900

    問題追加

---
+++ problems/abc249/src/d.rs
@@ -0,0 +1,12 @@
+use proconio::{input, marker::Chars};
+
+pub fn main() {
+    input! {
+        n: usize,
+        an: [i64; n]
+    };
+    // 全部の要素を素因数分解して考える
+    for i in 0..n {
+        // 素因数分解
+    }
+}
