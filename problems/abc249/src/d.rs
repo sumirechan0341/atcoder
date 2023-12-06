d0c46c866717c03ffd9c6ba98b061d2281c15fab
sumirechan0341 <mazeppa1108@yahoo.co.jp>
2023-12-06 18:43:26 +0900

    途中

--- problems/abc249/src/d.rs
+++ problems/abc249/src/d.rs
@@ -5,8 +5,27 @@ pub fn main() {
         n: usize,
         an: [i64; n]
     };
-    // 全部の要素を素因数分解して考える
+    let mut cnt = vec![0; 200001];
     for i in 0..n {
-        // 素因数分解
+        cnt[an[i] as usize] += 1;
     }
+    let mut ans = 0;
+    // 1だけのグループ
+    ans += (cnt[1] - 2).max(0) * (cnt[1] - 1).max(0) * cnt[1];
+    for i in 0..n {
+        for p in 2..=448 {
+            if p * p > an[i] {
+                break;
+            }
+            if an[i] % p == 0 {
+                if an[i] == p {
+                    ans += cnt[1] * (cnt[an[i] as usize] - 1) * 2;
+                } else {
+                    ans += cnt[p as usize] * cnt[(an[i] / p) as usize] * 2;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
